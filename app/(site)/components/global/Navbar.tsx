815760b2ac1caaba64adcfe3fe6775445bf74686
Briankim254 <kimutaibrian560@gmail.com>
2023-07-30 11:58:56 +0300

    simple changes

--- app/(site)/components/global/Navbar.tsx
+++ app/(site)/components/global/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <header className="py-6 md:px-16 px-6 border-b border-zinc-800 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
         <Link href="/">
-          <Image src={Logo} width={25} height={25} alt="logo" />
+          <Image src={Logo} width={50} height={50} alt="logo" />
         </Link>
         <nav>
           <ul className="flex items-center gap-x-8">
