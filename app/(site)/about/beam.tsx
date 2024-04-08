22022481559d995fae0ef68d64fbc61fb7ffa06c
Briankim254 <kimutaibrian560@gmail.com>
2024-04-08 17:07:21 +0300

    Update About page component and tailwind.config.js file

---
+++ app/(site)/about/beam.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { TracingBeam } from "../components/ui/tracing-beam";
+import Data from "./data";
+
+export default function TracingBeamWithData() {
+  return (
+    <TracingBeam className="px-6">
+      <Data />
+    </TracingBeam>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+ 
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +19,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
