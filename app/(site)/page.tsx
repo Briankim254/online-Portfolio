04bae9777b9877b90ba12484d3e8492965f0b2ac
Briankim254 <kimutaibrian560@gmail.com>
2024-04-08 16:03:23 +0300

    Add spinning globe and three-globe dependencies

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,13 +2,15 @@ import { getProfile } from "@/sanity/sanity.query";
 import type { ProfileType } from "@/types";
 import HeroSvg from "./icons/HeroSvg";
 import Job from "./components/Job";
+import GlobeDemo from "./components/AutoGlobe/page";
+
 
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
 
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
-      <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
+      <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between  lg:mt-32 mt-20 mb-16">
         {profile &&
           profile.map((data) => (
             <div key={data._id} className="lg:max-w-2xl max-w-2xl">
@@ -35,7 +37,8 @@ export default async function Home() {
               </ul>
             </div>
           ))}
-        <HeroSvg />
+        {/* <HeroSvg /> */}
+        <GlobeDemo />
       </section>
       <Job />
     </main>
